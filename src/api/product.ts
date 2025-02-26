c1c01483a8bd821c8dc3c215e7dba4907ca9795f
Tharindu Dhananjaya <herathhmtd.20@uom.lk>
2025-02-26 23:44:33 +0530

    Add product management functions for creating, deleting, and tagging products via WebSocket

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,10 +1,11 @@
-import { Message, Type } from "../types/types";
+import { Action, Message, Product, Type } from "../types/types";
 import { sendWSMessage } from "../utils/websocket-util";
 
 
 export async function getAllProducts(){
     const request:Message = {
-        type: Type.PRODUCT,
+        action: Action.PRODUCT_GET_ALL,
+        type: Type.REQUEST,
         message_id: '1',
         timestamp: Date.now().toString(),
     }
@@ -12,3 +13,44 @@ export async function getAllProducts(){
     sendWSMessage(request);
 }
 
+export async function createProduct(product: Product) {
+    const request: Message = {
+      action: Action.PRODUCT_ADD_EDIT,
+      type: Type.REQUEST,
+      message_id: Date.now().toString(),
+      timestamp: Date.now().toString(),
+      payload: {
+        products: [product],
+        timestamp: new Date().toISOString(),
+      },
+    };
+  
+    sendWSMessage(request);
+  }
+
+    export async function deleteProduct(productId: string) {
+        const request: Message = {
+        action: Action.PRODUCT_DELETE,
+        type: Type.REQUEST,
+        message_id: Date.now().toString(),
+        timestamp: Date.now().toString(),
+        payload: {
+            product_id: productId,
+            timestamp: new Date().toISOString(),
+        },
+        };
+    
+        sendWSMessage(request);
+    }
+
+    export async function writeTag(productId: string) {
+        const request: Message = {
+        action: Action.TAG_WRITE,
+        type: Type.REQUEST,
+        message_id: Date.now().toString(),
+        timestamp: Date.now().toString(),
+        payload: productId,
+        };
+    
+        sendWSMessage(request);
+    }
