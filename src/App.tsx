ce82b9db058e258979c2fa892302479d7086633f
Tharindu Dhananjaya <herathhmtd.20@uom.lk>
2025-02-26 23:41:19 +0530

    Refactor App component to update WebSocket initialization and message handling for new action types

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import './App.css';
-// import "antd/dist/reset.css";
-// import Inventory from "./pages/Inventory";
-// import Product from './pages/Products';
-// import { useEffect, useState } from 'react';
-// import initWS from './utils/websocket-util';
-// import { InventoryItem, Message, Product as ProductType, Type } from './types/types';
-
-
-// function App() {
-//   const [data, setData] = useState<any[]>([]);
-//   const [products, setProducts] = useState<ProductType[]>([]); 
-//   const [inventory, setInventory] = useState<InventoryItem[]>([]);
-  
-//   // Pass only the `url` and `setData` function to `initWS`
-//   useEffect(() => {
-//     initWS('ws://192.168.112.97:8000', setMessages);
-//   }, []);
-
-//   useEffect(() => {
-//     console.log('Data:', data);
-//   }, [data]);
-
-//   const setMessages = (message: Message) => {
-//     console.log('Message:', message);
-//     setData(prev=>[...prev, message]);
-//     switch (message.type) {
-//       case Type.PRODUCT:
-//         setProducts(message.data as ProductType[]);
-//         break;
-//       case Type.INVENTORY:
-//         setInventory(message.data as InventoryItem[]);
-//         break;
-//       default:
-//         break;
-//     }
-//   }
-
-//   return (
-//     <Router>
-//       <header style={{ backgroundColor: '#BACC81', padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', color: 'white', borderRadius: '5px' }}>
-//         <nav>
-//           <Link to="/" style={{ marginRight: '20px', color: 'white', textDecoration: 'none' }}>Products</Link>
-//           <Link to="/inventory" style={{ color: 'white', textDecoration: 'none' }}>Inventory</Link>
-//         </nav>
-//       </header>
-
-//       <main style={{ padding: '20px' }}>
-//         <Routes>
-//           <Route path="/" element={<Product items={products}/>} />
-//           <Route path="/inventory" element={<Inventory items={inventory}
-//             />} />
-//         </Routes>
-//       </main>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import "antd/dist/reset.css";
@@ -70,7 +5,7 @@ import Inventory from "./pages/Inventory";
 import Product from './pages/Products';
 import { useEffect, useState } from 'react';
 import initWS from './utils/websocket-util';
-import { InventoryItem, Message, Product as ProductType, Type } from './types/types';
+import { Action, InventoryItem, InventoryPayload, Message, ProductPayload, Product as ProductType, Type } from './types/types';
 
 function App() {
   const [data, setData] = useState<any[]>([]);
@@ -79,7 +14,7 @@ function App() {
 
   // Pass only the `url` and `setData` function to `initWS`
   useEffect(() => {
-    initWS('ws://192.168.112.97:8000', setMessages);
+    initWS('ws://192.168.112.14:8000', setMessages);
   }, []);
 
   useEffect(() => {
@@ -89,12 +24,12 @@ function App() {
   const setMessages = (message: Message) => {
     console.log('Message:', message);
     setData(prev => [...prev, message]);
-    switch (message.type) {
-      case Type.PRODUCT:
-        setProducts(message.data as ProductType[]);
+    switch (message.action) {
+      case Action.PRODUCT_GET_ALL:
+        setProducts((message.payload as ProductPayload).products || []);
         break;
-      case Type.INVENTORY:
-        setInventory(message.data as InventoryItem[]);
+      case Action.INVENTORY_GET_ALL:
+        setInventory((message.payload as InventoryPayload).inventory_items || []);
         break;
       default:
         break;
